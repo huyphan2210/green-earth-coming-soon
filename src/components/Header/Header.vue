c9a2b6b8e24f2716cdda90b461069e1bb349a380
Huy Phan <huy.phan2210@outlook.com>
2024-10-29 11:49:14 +0700

    Use Menu and toggle it

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
-import Logo from "../Logo/Logo.vue";
+import { ref } from "vue";
 import burgerMenu from "../../assets/burger-menu.svg";
+import Logo from "../Logo/Logo.vue";
+import Menu from "./Menu/Menu.vue";
+
+const isMenuShown = ref(false);
+const toggleMenu = () => {
+  isMenuShown.value = !isMenuShown.value;
+};
 </script>
 
 <template>
   <header>
     <Logo />
-    <button type="button" class="burger-menu">
+    <button type="button" class="burger-menu" :onclick="toggleMenu">
       <img :src="burgerMenu" loading="lazy" alt="Menu" />
     </button>
+    <Menu :toggle-menu="toggleMenu" v-if="isMenuShown" />
   </header>
 </template>
 
 <style lang="scss" scoped>
 header {
-  padding: 1rem;
+  padding: var(--padding);
   position: sticky;
   left: 0;
   top: 0;
