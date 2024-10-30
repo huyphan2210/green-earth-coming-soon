646cbf180658ee843aacebb3027fbc5b58e64f6c
Huy Phan <huy.phan2210@outlook.com>
2024-10-30 11:25:29 +0700

    Update App.vue
    Add Logo here

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "./components/Header/Header.vue";
 import Heading from "./components/Heading/Heading.vue";
+import Logo from "./components/Logo/Logo.vue";
 </script>
 
 <template>
+  <Logo />
   <Header />
   <main>
     <Heading />
