0040827ae38ca5dfc2bf1ef60ee9e7da7fdb3c9f
Huy Phan <huy.phan2210@outlook.com>
2024-10-31 11:47:30 +0700

    Style Heading

--- src/components/Heading/Heading.vue
+++ src/components/Heading/Heading.vue
@@ -1,7 +1,39 @@
 <template>
-  <!-- <h1>
-    <span>WE'RE</span>
-    <span>COMING</span>
-    <span>SOON</span>
-  </h1> -->
+  <h1>
+    <span class="top">WE'RE</span>
+    <span class="middle">COMING</span>
+    <span class="bottom">SOON</span>
+  </h1>
 </template>
+
+<style lang="scss" scoped>
+h1 {
+  font-family: "Nico Moji", sans-serif;
+  font-size: 3rem;
+  line-height: 1.5;
+  span {
+    display: inline-block;
+    &.top {
+      color: var(--lime-green);
+      position: relative;
+      float: right;
+      right: 0;
+      top: 0;
+    }
+
+    &.middle {
+      display: block;
+      color: var(--light-green);
+      text-align: center;
+      transform: rotate(-36deg);
+    }
+
+    &.bottom {
+      color: var(--green);
+      text-align: start;
+      left: 0;
+      top: 0;
+    }
+  }
+}
+</style>
