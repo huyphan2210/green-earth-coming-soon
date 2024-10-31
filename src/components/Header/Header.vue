6a52201dfe350c6269152d8e2cbce6228e7dc3f4
Huy Phan <huy.phan2210@outlook.com>
2024-10-31 11:47:22 +0700

    Update Header.vue

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import burgerMenu from "../../assets/burger-menu.svg";
 import Menu from "./Menu/Menu.vue";
+import Logo from "../Logo/Logo.vue";
 
 const isMenuShown = ref(false);
 const toggleMenu = () => {
@@ -11,6 +12,7 @@ const toggleMenu = () => {
 
 <template>
   <header>
+    <Logo />
     <button type="button" class="burger-menu" :onclick="toggleMenu">
       <img :src="burgerMenu" loading="lazy" alt="Menu" />
     </button>
@@ -25,10 +27,11 @@ header {
   left: 0;
   top: 0;
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: center;
   background-color: white;
   min-height: var(--header-height);
+  z-index: 1;
 
   .burger-menu {
     background-color: transparent;
@@ -37,6 +40,7 @@ header {
 
 @media screen and (min-width: 64rem) {
   header {
+    position: absolute;
     padding: 2.5rem 5rem;
     .burger-menu {
       display: none;
