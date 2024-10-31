31c2496a2e0aa3569ddd49053c6e310b73f7aa23
Huy Phan <huy.phan2210@outlook.com>
2024-10-31 11:47:17 +0700

    Add images + content section

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,67 @@
 <script setup lang="ts">
 import Header from "./components/Header/Header.vue";
 import Heading from "./components/Heading/Heading.vue";
-import Logo from "./components/Logo/Logo.vue";
 </script>
 
 <template>
-  <Logo />
   <Header />
   <main>
-    <Heading />
+    <section class="image-wrapper">
+      <img
+        class="image"
+        src="./assets/images/image-mobile.png"
+        loading="lazy"
+        alt="Image"
+      />
+      <img
+        class="image desktop"
+        src="./assets/images/image-desktop.png"
+        loading="lazy"
+        alt="Image"
+      />
+    </section>
+    <section class="content">
+      <Heading />
+    </section>
   </main>
   <footer></footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  .image-wrapper {
+    display: flex;
+    .image {
+      width: 100%;
+      &.desktop {
+        display: none;
+      }
+    }
+  }
+
+  .content {
+    padding-inline: calc(var(--padding) * 2);
+  }
+}
+
+@media screen and (min-width: 64rem) {
+  main {
+    display: flex;
+    flex-direction: row-reverse;
+    flex: 1;
+    .image-wrapper {
+      width: 40%;
+      .image {
+        display: none;
+        &.desktop {
+          display: inline;
+        }
+      }
+    }
+
+    .content {
+      width: calc(60% - var(--padding) * 2);
+    }
+  }
+}
+</style>
